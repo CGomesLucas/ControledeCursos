3b991effb84b57d45245ff8c937937d57e6ec839
CGomesLucas <cruzlucasads1@gmail.com>
2026-05-23 00:17:14 -0300

    fix: a lot of things fixed. I don´t care about the commit, my API is finally working WOOWWWWW

--- app/models/course_model.py
+++ app/models/course_model.py
@@ -3,23 +3,14 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.core.database import Base
 
-class AuthorModel(Base):
-    __tablename__ = "authors"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    age = Column(Integer, nullable=False)
-    course_id = Column(Integer, ForeignKey("courses.id", ondelete="CASCADE"))
-    course = relationship("CourseModel", back_populates="authors")
-
 class CourseModel(Base):
     __tablename__ = "courses"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
+    price = Column(String, nullable=False)
     description = Column(String, nullable=True) 
     course_hours = Column(Integer, nullable=False)
     activated = Column(Boolean, default=True)
-    created_at = Column(DateTime, default=datetime) 
+    created_at = Column(DateTime, default=datetime.now) 
     related_topics = Column(JSON, default=list)
-    authors = relationship("AuthorModel", back_populates="course", cascade="all, delete-orphan")
