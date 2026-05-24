e219700a3acae4921195978042b91f79fcf7cd40
CGomesLucas <cruzlucasads1@gmail.com>
2026-05-24 16:01:26 -0300

    fix: CourseService and Course Repository fixed

--- app/routers/course_routes.py
+++ app/routers/course_routes.py
@@ -1,58 +1,30 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from app.schemas.course_schema import CourseCreate, CourseResponse, CourseUpdate
 from sqlalchemy.orm import Session
-from app.models.course_model import CourseModel
 from app.core.database import get_db
+from app.services import course_service, user_service
 
 router = APIRouter(prefix="/courses", tags=["Courses"])
 
 @router.get("/", response_model=list[CourseResponse])
 def findAll_Courses(db: Session = Depends(get_db)):
-    cursos = db.query(CourseModel).all()
-    return cursos
+
 
 @router.get("/{course_id}", response_model=CourseResponse)
 def findById_Courses(course_id: int, db: Session = Depends(get_db)):
-    curso = db.query(CourseModel).filter(CourseModel.id == course_id).first()
-    if not curso:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
-    return curso
+
 
 @router.post("/", response_model=CourseResponse, status_code=status.HTTP_201_CREATED)
 def create_course(course: CourseCreate, db: Session = Depends(get_db)):
-    new_course = CourseModel(**course.model_dump(exclude={"author"}))
 
-    db.add(new_course)
-    db.commit()
-    db.refresh(new_course)
-
-    return new_course
 
 @router.put("/{course_id}", response_model=CourseResponse)
 def update_course(course_id: int, course: CourseUpdate, db: Session = Depends(get_db)):
-    new_course = db.query(CourseModel).filter(CourseModel.id == course_id).first()
-    if not new_course:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
 
-    for key, value in course.model_dump(exclude_unset=True).items():
-        setattr(new_course, key, value)
-
-    db.commit()
-    db.refresh(new_course)
-
-    return new_course
     
 
 @router.delete("/{course_id}")
 def delete_course(course_id: int, db: Session = Depends(get_db)):
-    course = db.query(CourseModel).filter(CourseModel.id == course_id).first()
 
-    if not course:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
-    
-    db.delete(course)
-    db.commit()
-        
-    return {"message": "Curso deletado com sucesso"}
 
 
--- app/services/course_service.py
+++ app/services/course_service.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException, status
+from typing import Sequence
 from app.repositories.course_repository import CourseRepository
 from app.models.course_model import CourseModel
 from app.schemas.course_schema import CourseCreate, CourseUpdate
@@ -8,23 +9,20 @@ class CourseService:
     def __init__(self):
         self.repository = CourseRepository()
     
-    def get_all_courses(self, db: Session) -> list[CourseModel]:
+    def get_all_courses(self, db: Session) -> Sequence[CourseModel]:
         return self.repository.findAll_courses(db)
     
     def get_course_by_id(self, course_id: int, db: Session) -> CourseModel:
-        course = self.repository.find_by_id(course_id, db)
+        course = self.repository.findById_courses(course_id, db)
         if not course:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
         
         return course
     
-    def create_course(self, course_id: int, course_data: CourseCreate, db: Session) -> CourseModel:
-        existing_course = self.repository.findById_courses(course_id, db)
-        if existing_course:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Curso já cadastrado")
-        
-        course_model = CourseModel(**course_data.model.dump())
-        return self.repository.create_courses(course_model, db)
+    def create_course(self, course_data: CourseCreate, db: Session) -> CourseModel:
+        course = CourseModel(**course_data.model_dump())
+
+        return self.repository.create_courses(course, db)
     
     def update_course(self, course_id: int, course_data: CourseUpdate, db: Session) -> CourseModel:
         course = self.repository.findById_courses(course_id, db)
@@ -32,12 +30,12 @@ class CourseService:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
         
         update_course_data = course_data.model_dump(exclude_unset=True)
-        for c, v in self.update_course.items():
+        for c, v in update_course_data.items():
             setattr(course, c, v)
         
         return self.repository.update_courses(course, db)
     
-    def delete_course(self, course_id: int, db: Session) -> CourseModel:
+    def delete_course(self, course_id: int, db: Session) -> None:
         course = self.repository.findById_courses(course_id, db)
         if not course:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
