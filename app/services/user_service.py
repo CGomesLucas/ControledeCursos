be3369003a4abe16d0d051a1bdcf6d1b33973b1a
CGomesLucas <cruzlucasads1@gmail.com>
2026-05-24 15:30:43 -0300

    setup create and implementing user_service

---
+++ app/services/user_service.py
@@ -0,0 +1,50 @@
+from fastapi import HTTPException, status
+from typing import Sequence
+from app.models.user_model import UserModel
+from sqlalchemy.orm import Session
+from app.schemas.user_schema import CreateUser, UpdateUser
+from app.repositories.user_repository import UserRepository
+
+
+class UserService:
+    def __init__(self):
+        self.repository = UserRepository()
+    
+    def get_all_users(self, db: Session) -> Sequence[UserModel]:
+        return self.repository.findAll_users(db)
+    
+    def get_users_by_id(self, user_id: int, db: Session) -> UserModel:
+        return self.repository.findById_users(user_id, db)
+    
+    def create_user(self, user_data: CreateUser, db: Session) -> UserModel:
+        user = UserModel(**user_data.model_dump())
+        
+        return self.repository.create_users(user, db)
+    
+    def update_user(self, user_id: int, user_data: UpdateUser, db: Session) -> UserModel:
+        user = self.repository.findById_users(user_id, db)
+
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
+        
+        user_update_data = UserModel(user_data.model_dump(exclude_unset=True))
+        for c, v in user_data.items():
+            setattr(user_update_data, c, v)
+        
+        return self.repository.update_users(user_update_data, db)
+    
+    def delete_user(self, user_id: int, db: Session) -> None:
+        user = self.repository.findById_users(user_id, db)
+
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
+        
+        return self.repository.delete_users(user, db) 
+
+        
+
+
+    
+
+    
+        
