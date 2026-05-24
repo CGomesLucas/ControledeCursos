216cf2d3595cbcbc2030c69e1313c87c8afa2e98
CGomesLucas <cruzlucasads1@gmail.com>
2026-05-24 16:11:10 -0300

    feat: setup implementing services to User and Course Routes

--- app/routers/course_routes.py
+++ app/routers/course_routes.py
@@ -1,30 +1,35 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from app.schemas.course_schema import CourseCreate, CourseResponse, CourseUpdate
 from sqlalchemy.orm import Session
 from app.core.database import get_db
-from app.services import course_service, user_service
+from app.services.course_service import CourseService
 
 router = APIRouter(prefix="/courses", tags=["Courses"])
 
 @router.get("/", response_model=list[CourseResponse])
 def findAll_Courses(db: Session = Depends(get_db)):
-
+    service = CourseService()
+    return service.CourseService.get_all_courses(db)
 
 @router.get("/{course_id}", response_model=CourseResponse)
 def findById_Courses(course_id: int, db: Session = Depends(get_db)):
-
-
+    service = CourseService()
+    return service.CourseService.get_course_by_id(course_id, db)
+    
 @router.post("/", response_model=CourseResponse, status_code=status.HTTP_201_CREATED)
 def create_course(course: CourseCreate, db: Session = Depends(get_db)):
-
+    service = CourseService()
+    return service.CourseService.create_course(course, db)
 
 @router.put("/{course_id}", response_model=CourseResponse)
 def update_course(course_id: int, course: CourseUpdate, db: Session = Depends(get_db)):
-
-    
+    service = CourseService()
+    return service.CourseService.update_course(course_id, course, db) 
 
 @router.delete("/{course_id}")
 def delete_course(course_id: int, db: Session = Depends(get_db)):
+    service = CourseService()
+    return service.CourseService.delete_course(course_id, db)
 
 
 
--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -1,61 +1,33 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from app.schemas.user_schema import CreateUser, ResponseUser, UpdateUser
-from app.models.user_model import UserModel
+from app.services.user_service import UserService
 from app.core.database import get_db
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/", response_model=list[ResponseUser])
 def findAll_users(db: Session = Depends(get_db)):
-    users = db.query(UserModel).all()
-    if not users:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuários não encontrados")
-
-    return users
+    service = UserService()
+    return service.get_all_users(db)
 
 @router.get("/{user_id}", response_model=ResponseUser)
 def findById_users(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(UserModel).filter(UserModel.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
-
-    return user
+    service = UserService()
+    return service.get_users_by_id(user_id, db)
 
 @router.put("/{user_id}", response_model=ResponseUser)
 def update_users(user_id: int, user: UpdateUser, db: Session = Depends(get_db)):
-    new_user = db.query(UserModel).filter(UserModel.id == user_id).first()
-    if not new_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
-
-    for key, value in user.model_dump(exclude_unset=True).items():
-        setattr(new_user, key, value)
-
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
+    service = UserService()
+    return service.update_user(user_id, user, db)
 
 @router.post("/", response_model=ResponseUser, status_code=status.HTTP_201_CREATED)
 def create_users(user: CreateUser, db: Session = Depends(get_db)):
-    new_user = UserModel(**user.model_dump())
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
+    service = UserService()
+    return service.create_user(user, db)
 
 @router.delete("/{user_id}")
 def delete_users(user_id: int,db: Session = Depends(get_db)):
-    user = db.query(UserModel).filter(UserModel.id == user_id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
-    
-    db.delete(user)
-    db.commit()
-
-    return {"message": "Usuário deletado com sucesso"}
-    
+    service = UserService()
+    return service.delete_user(user_id, db)
 
