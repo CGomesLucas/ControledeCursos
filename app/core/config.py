97fc2f5a9cb1d1b34ef386649c3ff91d8ceedcd4
CGomesLucas <cruzlucasads1@gmail.com>
2026-05-24 23:01:29 -0300

    fix: fixing config and .env

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     secret_key: str
     access_token_expire_minutes: int
-    access_token_expire_days: int
+    refresh_token_expire_days: int
     algorithm: str
 
-model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
