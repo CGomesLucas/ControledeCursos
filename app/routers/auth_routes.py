08803e6b740de6ac3c1ba4fc535dd6f6bd4fa7d3
CGomesLucas <cruzlucasads1@gmail.com>
2026-05-26 08:34:43 -0300

    feat: setup creating Auth archives and started to implement Auth_Repository

---
+++ app/routers/auth_routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException 
+from sqlalchemy.orm import Session
+from app.core.database import get_db
+from schemas.auth_schema import LoginResponseUserInfo, LoginResponseTokens, LoginRequest, ResponseRefreshToken, RequestRefreshToken
+
+app = APIRouter(prefix="/auths", tags="Auths")
+
+@app.post("/login")
+def login(body: LoginRequest, db: Session = Depends(get_db)):
+    
+
+
+
+
+
+
